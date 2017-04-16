ca81b8651ef5da01cddd66bcd5be4f2fcd04cec5
Matthew Pressman <mrp3362@rit.edu>
2017-04-16 17:47:18 -0400

    Added Board Structure and Corrected Project Structure

--- Project/Shuffleboard/Board.h
+++ Project/Shuffleboard/Board.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include "RE\ReEngAppClass.h"
+#include "RE\ReEng.h"
+#include "MyPrimitive.h"
+
+using namespace ReEng;
+
+class Board {
+private:
+	float m_fwidth;
+	float m_fheight;
+	float m_fsize;
+	vector3 m_v3Position;
+	std::vector<PrimitiveClass> m_vpcolliders;
+	MeshManagerSingleton* m_pMeshMngr = nullptr;
+
+public:
+	Board(vector3 a_v3Position);
+	Board(vector3 a_v3Position, float a_size);
+	Board(Board &other);
+	~Board();
+
+	int CalculatePoints();
+	void Render();
+};
