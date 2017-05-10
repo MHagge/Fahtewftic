576be5b542ba6e3d1b0f6735df9424e29fbcf7ab
Nathaniel Sun (RIT Student) <njs6145@ad.rit.edu>
2017-05-10 17:27:05 -0400

    Scoring set up

--- Project/Shuffleboard/Board.h
+++ Project/Shuffleboard/Board.h
@@ -3,6 +3,7 @@
 #include "RE\ReEngAppClass.h"
 #include "RE\ReEng.h"
 #include "MyPrimitive.h"
+#include "MyBOManager.h"
 
 using namespace ReEng;
 
@@ -15,14 +16,26 @@ private:
 	std::vector<PrimitiveClass*> m_vpPlanes;
 	std::vector<matrix4> m_vm4WorldMats;
 
+	MyBOManager* boManagerObj = nullptr;
+	
+
+
 public:
 	Board();
 	Board(vector3 a_v3Position);
 	Board(vector3 a_v3Position, float a_size);
 	Board(Board &other);
+
+	Board& Board::operator=(Board const& other);
+
 	void DeleteBoard();
 	void Init();
 
 	int CalculatePoints();
 	void Render(matrix4 a_m4Proj, matrix4 a_mView);
+
+	void SetPosition(vector3 a_v3Input);
+
+	std::vector<String> m_vsNames;
+	std::vector<std::vector<int>> boardCollisions;
 };
--- Project/Shuffleboard/GameManager.cpp
+++ Project/Shuffleboard/GameManager.cpp
@@ -5,7 +5,10 @@ void GameManager::Init(void)
 {
 	m_pMeshMngr = MeshManagerSingleton::GetInstance();
 	m_pBOMngr = MyBOManager::GetInstance();
-	m_bBoard = Board(vector3(0, 0, -10));
+
+	m_bBoard.SetPosition(vector3(0, 0, -10));
+	m_bBoard.Init();
+
 	m_nPucks = 0;
 	m_lPucks = std::vector<Puck>();
 	m_lPuckNames = std::vector<std::string>();
@@ -15,6 +18,9 @@ void GameManager::Release(void)
 	m_lPucks.clear();
 	m_lPuckNames.clear();
 	m_lModelMatrices.clear();
+
+	m_bBoard.DeleteBoard();
+
 }
 GameManager* GameManager::GetInstance()
 {
@@ -34,30 +40,54 @@ void GameManager::ReleaseInstance()
 }
 void GameManager::RenderObjects(matrix4 a_m4Proj, matrix4 a_m4View)
 {
-	m_pMeshMngr->AddInstanceToRenderList(m_lPuckNames);
+
 	m_bBoard.Render(a_m4Proj, a_m4View);
 }
-void GameManager::AddNewPuck()
+void GameManager::AddNewPuck(bool a_bearth)
 {
-	Puck newPuck = Puck(m_lPuckNames[m_lPuckNames.size() - 1] + std::to_string(m_lPuckNames.size()), vector3(0, 0, 0));
+	Puck newPuck;
+	if (m_lPuckNames.size() == 0) {
+		newPuck = Puck("First", vector3(0, 0, 0));
+	}
+	else {
+		newPuck = Puck(m_lPuckNames[m_lPuckNames.size() - 1] + std::to_string(m_lPuckNames.size()), vector3(0, 0, 0));
+	}
+	 
 	m_lPucks.push_back(newPuck);
 	m_lPuckNames.push_back(newPuck.GetName());
 	m_lModelMatrices.push_back(matrix4(0));
+	m_nPucks++;
 	m_pBOMngr->AddObject(newPuck.GetName());
+	if (a_bearth) {
+		m_pMeshMngr->LoadModel("Planets\\03_Earth.obj", newPuck.GetName());
+	}
+	else {
+		m_pMeshMngr->LoadModel("Planets\\03A_Moon.obj", newPuck.GetName());
+	}
+	
 }
-void GameManager::AddNewPuck(Puck a_puNewPuck)
+void GameManager::AddNewPuck(bool a_bearth, Puck a_puNewPuck)
 {
 	m_lPucks.push_back(a_puNewPuck);
 	m_lPuckNames.push_back(a_puNewPuck.GetName());
 	m_lModelMatrices.push_back(matrix4(0));
+
 	m_pBOMngr->AddObject(a_puNewPuck.GetName());
 }
-void GameManager::AddNewPuck(Puck a_puNewPuck, matrix4 a_m4Model)
+void GameManager::AddNewPuck(bool a_bearth, Puck a_puNewPuck, matrix4 a_m4Model)
 {
 	m_lPucks.push_back(a_puNewPuck);
 	m_lPuckNames.push_back(a_puNewPuck.GetName());
 	m_lModelMatrices.push_back(a_m4Model);
+	m_nPucks++;
 	m_pBOMngr->AddObject(a_puNewPuck.GetName());
+	if (a_bearth) {
+		m_pMeshMngr->LoadModel("Planets\\03_Earth.obj", a_puNewPuck.GetName());
+	}
+	else {
+		m_pMeshMngr->LoadModel("Planets\\03A_Moon.obj", a_puNewPuck.GetName());
+	}
+
 }
 void GameManager::SetModelMatrix(int a_nIndex, matrix4 a_m4Model)
 {
@@ -65,11 +95,13 @@ void GameManager::SetModelMatrix(int a_nIndex, matrix4 a_m4Model)
 }
 void GameManager::Update() {
 	for (uint i = 0; i < m_lModelMatrices.size(); i++) {
+		m_lModelMatrices[i] *= glm::translate(m_lPucks[i].GetPosition());
+
 		m_pMeshMngr->SetModelMatrix(m_lModelMatrices[i], m_lPuckNames[i]);
 		m_pBOMngr->SetModelMatrix(m_lModelMatrices[i], m_lPuckNames[i]);
 	}
 	m_pBOMngr->Update();
-	for (uint i = 0; i < m_pBOMngr->GetIndexSize()- 1; i++) {
+	for (uint i = 0; i < m_pBOMngr->GetIndexSize() - 1; i++) {
 
 		//For each index collision, also get the object collided with
 		//Get all objects collided from colliding indices list
@@ -82,9 +114,42 @@ void GameManager::Update() {
 			collisions.push_back(indicesB);
 		}
 	}
+}
 
 
+matrix4 GameManager::GetModelMatrix(int a_nIndex)
+{
+	return m_lModelMatrices[a_nIndex];
+}
+void GameManager::SetPuckByIndex(int a_nIndex, Puck a_puNew)
+{
+	m_lPucks[a_nIndex] = a_puNew;
+}
+Puck GameManager::GetPuckByIndex(int a_nIndex)
+{
+	return m_lPucks[a_nIndex];
+}
+void GameManager::Scoring()
+{
+	int p1Score, p2Score = 0;
+	for (int i = 0; i < m_bBoard.m_vsNames.size(); i++) {
+		boardCollisions.push_back(m_pBOMngr->GetCollidingVector(m_bBoard.m_vsNames[i]));
+	}
 
+	for (int i = 0; i < boardCollisions.size(); i++) {
+		
+		//if()
+
+	}
+
+}
+void GameManager::AddInstances()
+{
+	m_pMeshMngr->AddInstanceToRenderList("ALL");
+}
+int GameManager::GetNumOfPucks()
+{
+	return m_nPucks;
 }
 //The big 3
 GameManager::GameManager() { Init(); }
